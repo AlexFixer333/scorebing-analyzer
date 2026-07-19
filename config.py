7d237ee9518e06984cd17c9b945415544a152f23
AlexFixer333 <blackboxbet@gmail.com>
2026-07-19 23:07:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-BASE_URL = "https://scorimer.com/ru/"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
-}
+# API-Football через RapidAPI
+API_URL = "https://v3.football.api-sports.io"
+API_KEY = "2107adcf998a66f1c7e30ec3f1415b02"  # <-- Вставьте ваш ключ!
 OUTPUT_DIR = "data"
