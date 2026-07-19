2710c0fdd62acd03adb54b0cc89ef121bbcc13c2
AlexFixer333 <blackboxbet@gmail.com>
2026-07-19 20:24:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://www.scorebing.com/"
+BASE_URL = "https://scorimer.com/ru/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
